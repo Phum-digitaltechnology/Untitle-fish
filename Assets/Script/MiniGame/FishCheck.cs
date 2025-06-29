a7014e270e095030e62d81758c2a6cbf39942cfa
Suthanon <suthanonv@gmail.com>
2025-06-29 22:06:10 +0700

    [MG_8 Update] Random Fish pond Pos

--- Assets/Script/MiniGame/FishCheck.cs
+++ Assets/Script/MiniGame/FishCheck.cs
@@ -10,6 +10,16 @@ public class FishCheck : MonoBehaviour
     [SerializeField] UnityEvent FoundFish;
     [SerializeField] UnityEvent NotFoundFish;
 
+    [Header("Pos Random")]
+    [SerializeField] Transform pos1;
+    [SerializeField] Transform pos2;
+
+
+    [ContextMenu("Test Random")]
+    public void SetUp()
+    {
+        this.transform.position = new Vector3(Random.Range(pos1.transform.position.x, pos2.transform.position.x), this.transform.position.y, this.transform.position.z);
+    }
 
     public void IsFoundFish()
     {
