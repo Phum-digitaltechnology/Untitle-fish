c396579e2d58258ba17da5ceb6fe9d16d5e4fc8a
Phumiphat <122847294+Phum-digitaltechnology@users.noreply.github.com>
2025-07-08 23:32:11 +0700

    fix Mini game3

--- Assets/GrillFishMinigame.cs
+++ Assets/GrillFishMinigame.cs
@@ -3,10 +3,26 @@ using UnityEngine.Events;
 
 public class GrillFishMinigame : MonoBehaviour
 {
+    [Header("Line Control")]
     public HeatLineMover lineMover;
-    public Collider2D cookedZone;
     public float gameDuration = 5f;
 
+    [Header("Zone Colliders")]
+    public Collider2D rawZone;
+    public Collider2D mediumZone;
+    public Collider2D cookedZone;
+    public Collider2D burnedZone;
+
+    [Header("Fish Sprite by Doneness")]
+    public Sprite rawFishSprite;
+    public Sprite mediumFishSprite;
+    public Sprite cookedFishSprite;
+    public Sprite burnedFishSprite;
+
+    [Header("Fish Renderer")]
+    public SpriteRenderer fishRenderer;
+
+    [Header("Events")]
     public UnityEvent OnWin;
     public UnityEvent OnLose;
 
@@ -18,6 +34,7 @@ public class GrillFishMinigame : MonoBehaviour
     {
         timer = gameDuration;
         gameStarted = true;
+        Debug.Log("MiniGame Start");
     }
 
     private void Update()
@@ -35,6 +52,8 @@ public class GrillFishMinigame : MonoBehaviour
             lineMover.StopMoving();
         }
 
+        UpdateFishSprite();
+
         if (timer <= 1f && !resultChecked)
         {
             resultChecked = true;
@@ -47,16 +66,40 @@ public class GrillFishMinigame : MonoBehaviour
         }
     }
 
+    private void UpdateFishSprite()
+    {
+        Collider2D lineCol = lineMover.GetComponent<Collider2D>();
+
+        if (lineCol.IsTouching(rawZone))
+        {
+            fishRenderer.sprite = rawFishSprite;
+        }
+        else if (lineCol.IsTouching(mediumZone))
+        {
+            fishRenderer.sprite = mediumFishSprite;
+        }
+        else if (lineCol.IsTouching(cookedZone))
+        {
+            fishRenderer.sprite = cookedFishSprite;
+        }
+        else if (lineCol.IsTouching(burnedZone))
+        {
+            fishRenderer.sprite = burnedFishSprite;
+        }
+    }
+
     private void CheckResult()
     {
         Collider2D lineCol = lineMover.GetComponent<Collider2D>();
 
         if (lineCol.IsTouching(cookedZone))
         {
+            Debug.Log("WIN: Cooked Level!");
             OnWin?.Invoke();
         }
         else
         {
+            Debug.Log("LOSE: Not Cooked!");
             OnLose?.Invoke();
         }
     }
--- Assets/HeatLineMover.cs
+++ Assets/HeatLineMover.cs
@@ -4,6 +4,7 @@ public class HeatLineMover : MonoBehaviour
 {
     public float speed = 2f;
     private bool isMoving = false;
+    public Transform rightEdge;
 
     public void StartMoving()
     {
@@ -19,7 +20,16 @@ public class HeatLineMover : MonoBehaviour
     {
         if (isMoving)
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Vector3 newPosition = transform.position + Vector3.right * speed * Time.deltaTime;
+
+            if (newPosition.x <= rightEdge.position.x)
+            {
+                transform.position = newPosition;
+            }
+            else
+            {
+                transform.position = new Vector3(rightEdge.position.x, transform.position.y, transform.position.z);
+            }
         }
     }
 }
