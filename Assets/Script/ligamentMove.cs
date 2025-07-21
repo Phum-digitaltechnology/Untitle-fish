1415bb09f2dc3ecb14507050c8461d8c01bcb777
Suthanon <suthanonv@gmail.com>
2025-07-22 06:11:40 +0700

    [Update] MG8Fishing Visual + Merge Visual From MG8

--- Assets/Script/ligamentMove.cs
+++ Assets/Script/ligamentMove.cs
@@ -1,34 +1,54 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 public class ligamentMove : MonoBehaviour
 {
 
-    [SerializeField] float moveTime;
+    [SerializeField] float moveSpeed;
     [SerializeField] Transform destination;
+    [SerializeField] float OffSetY;
     [SerializeField] Transform objToMove;
     float currentTime = 0;
-    Vector3 startPos;
+    Vector3 startPos, endPos;
+    [SerializeField] UnityEvent onFinishMove;
+    [SerializeField] float TbeforeMove;
+    [SerializeField] UnityEvent beforeFinish;
     [ContextMenu("Debug Move")]
     public void StartMove()
     {
+        endPos = destination.position;
+        endPos = new Vector3(destination.position.x, destination.position.y + OffSetY, destination.position.z);
         startPos = objToMove.position;
         StartCoroutine(CurveMove());
     }
+    bool isplayed = false;
     IEnumerator CurveMove()
     {
-        float t = 0;
-        while (t <= 1)
-        {
+        Vector3 startPos = objToMove.transform.position;
+        float totalDistance = Vector3.Distance(startPos, endPos);
+        float traveledDistance = 0f;
 
-            Vector3 finalPos = Vector3.Lerp(objToMove.transform.position, destination.transform.position, t);
-            Vector3 yPos = Vector3.Slerp(objToMove.transform.position, destination.transform.position, t);
-            finalPos.y = yPos.y;
-            objToMove.transform.position = finalPos;
-            currentTime += Time.deltaTime;
-            t = currentTime / moveTime;
+        while (traveledDistance < totalDistance)
+        {
+            traveledDistance += moveSpeed * Time.deltaTime;
+            float t = Mathf.Clamp01(traveledDistance / totalDistance);
+            Vector3 straightPos = Vector3.Lerp(startPos, endPos, t);
+            Vector3 curvePos = Vector3.Slerp(startPos, endPos, t);
+            straightPos.y = curvePos.y;
+            objToMove.transform.position = straightPos;
+
+            if (t >= TbeforeMove && isplayed == false)
+            {
+                isplayed = true;
+                beforeFinish?.Invoke();
+            }
+
             yield return null;
         }
+        onFinishMove?.Invoke();
+        objToMove.transform.position = endPos; // Snap to exact final position
     }
-
-
 }
+
+
+
