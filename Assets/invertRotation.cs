c25fb4b5f379a1ac526fa907eafb2dede82e448b
matt2341 <aphiphu.m13@gmail.com>
2025-07-24 12:44:31 +0700

    update something

--- Assets/invertRotation.cs
+++ Assets/invertRotation.cs
@@ -12,11 +12,15 @@ public class invertRotation : MonoBehaviour
     {
         if (Invert)
         {
+            if (this.transform.rotation.z == 180) return;
+
             Vector3 angles = transform.eulerAngles;
             transform.rotation = Quaternion.Euler(angles.x, angles.y, 180);
         }
         else
         {
+            if (this.transform.rotation.z == 0) return;
+
             Vector3 angles = transform.eulerAngles;
             transform.rotation = Quaternion.Euler(angles.x, angles.y, 0);
         }
