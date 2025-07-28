1cf9a069643d9b39726114e7817e2e3d44454b14
nmmn4947 <nmmn4947@gmail.com>
2025-07-29 03:28:11 +0700

    pauseNotDone

--- Assets/Script/MainMenu/Button_StartGame.cs
+++ Assets/Script/MainMenu/Button_StartGame.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Button_StartGame : MonoBehaviour
 {
     public string SceneName;
+    public Slider master;
+    public Slider music;
+    public Slider sfx;
+
+    private void Start()
+    {
+        master.value = PlayerPrefs.GetFloat("MasterVolume");
+        music.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfx.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
 
     public void StartGame()
     {
@@ -13,12 +24,30 @@ public class Button_StartGame : MonoBehaviour
 
     public void ExitGame()
     {
-        Application.Quit();
-        Debug.Log("Off we gooooooo");
+        //Application.Quit();
+        //Debug.Log("Off we gooooooo");
+        SceneManager.LoadScene("MainMenu");
     }
 
-    public void Credits()
+    private void Update()
     {
+        SetMasterVolume(master.value);
+        SetMusicVolume(music.value);
+        SetSFXVolume(sfx.value);
+    }
 
+    public void SetMasterVolume(float t)
+    {
+        AudioManager.Instance.SetMasterVolume(t);
+    }
+
+    public void SetMusicVolume(float t)
+    {
+        AudioManager.Instance.SetMusicVolume(t);
+    }
+
+    public void SetSFXVolume(float t)
+    {
+        AudioManager.Instance.SetSFXVolume(t);
     }
 }
