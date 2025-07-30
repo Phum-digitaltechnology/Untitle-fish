4d4b53bf44105f3259512c10bf5a6a1fa8238236
NeedHelp <rachatafewfew@gmail.com>
2025-07-31 01:51:25 +0700

    [Update] I am a dumbass!

--- Assets/Script/MiniGame/FishTugMinigame.cs
+++ Assets/Script/MiniGame/FishTugMinigame.cs
@@ -125,13 +125,9 @@ public class FishTugMinigame : MonoBehaviour
 
         fish.position += Vector3.right * fishDir * currentSpeed * Time.deltaTime;
 
-        if (fishCollider.IsTouching(loseZone)) //gameTimer > 2f && 
+        if (fishCollider.IsTouching(loseZone))
         {
             Finish(false);
-        }
-        else
-        {
-            Finish(true);
         }
     }
 
